29a01a7703e606b1aaa604292a90e5970b7e3f05
shivamsoni-in <152253499+shivamsoni-in@users.noreply.github.com>
2023-11-28 12:55:40 +0530

    Update db-connection.php

--- db-connection.php
+++ db-connection.php
@@ -1,13 +1,1 @@
-<?php
-
-    $con = mysqli_connect("localhost","root","","wings_wingspharma");
-
-    function get_safe_value($con,$str){
-        if($str!=''){
-            $str = trim($str);
-            return strip_tags(mysqli_real_escape_string($con,$str));
-        }
-    }
-
-?>
-
+404
